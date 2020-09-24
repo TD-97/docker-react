a042a7c0f7f141081b73a75dab7f0aebbda1de95
Tom <thomasdavis083@gmail.com>
2020-09-24 13:29:46 +0100

    updated app on feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p className="App-intro">
-          Sure look
+          Hi Conor Davis, Tom Davis Created this, hosting on AWS
         </p>
         <a
           className="App-link"
